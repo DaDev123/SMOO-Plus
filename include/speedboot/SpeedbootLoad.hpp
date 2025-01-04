89e646e11ada51de75275b8c532f355cefd956b5
Secret Dev <131434750+DaDev123@users.noreply.github.com>
2025-01-04 17:22:00 +0100

    Add files via upload

--- include/speedboot/SpeedbootLoad.hpp
+++ include/speedboot/SpeedbootLoad.hpp
@@ -1,7 +1,6 @@
 #include "al/layout/LayoutActor.h"
 #include "al/layout/LayoutInitInfo.h"
 #include "al/util/NerveUtil.h"
-#include "math/seadVector.h"
 
 class WorldResourceLoader;
 
@@ -14,27 +13,8 @@ public:
     void exeDecrease();
     void exeEnd();
 
-    float mTime = 0.f;
     float mProgression = 0.f;
     float mRotTime = 0.f;
-
-    // Online logo part
-    float mOnlineLogoScale = 0.f;
-    float mOnlineLogoScaleTarget = 0.f;
-    sead::Vector2f mOnlineLogoTrans = sead::Vector2f::zero;
-    sead::Vector2f mOnlineLogoTransTarget = sead::Vector2f::zero;
-
-    // Freze tag logo root
-    float mFreezeLogoTransX = 1000.f;
-    float mFreezeLogoTransXTarget = 1000.f;
-
-    // Borders
-    sead::Vector2f mFreezeBorder = {700.f, 420.f};
-    sead::Vector2f mFreezeBorderTarget = {700.f, 420.f};
-
-    // Backgrounds
-    float mFreezeBGTransX = 0.f;
-    float mFreezeBGTransXTarget = 0.f;
 
 private:
     WorldResourceLoader* worldResourceLoader;
