7544bfd8e3026ae144589537a8ee73ef41157bac
Secret Dev <131434750+DaDev123@users.noreply.github.com>
2025-01-23 17:27:16 +0100

    Potential Freeze Tag packet-fixes #7

--- include/packets/FreezeInf.h
+++ include/packets/FreezeInf.h
@@ -3,6 +3,20 @@
 #include "Packet.h"
 #include "sead/basis/seadTypes.h"
 
+enum FreezeUpdateType : u8 { // Type of packets to send between players
+    PLAYER                 = 1 << 0,
+    ROUNDSTART             = 1 << 1,
+    ROUNDCANCEL            = 1 << 2,
+    FALLOFF                = 1 << 3
+};
+
+enum FreezePostProcessingType : u8 { // Snapshot mode post processing state
+    PPDISABLED = 0,
+    PPFROZEN = 1,
+    PPENDGAMELOSE = 2,
+    PPENDGAMEWIN = 3
+};
+
 struct PACKED FreezeInf : Packet {
     FreezeInf() : Packet() { this->mType = PacketType::TAGINF; mPacketSize = sizeof(FreezeInf) - sizeof(Packet);};
     TagUpdateType updateType;
