9d952b6b99563213e119dd47ff6c0e30a75bf9f6
Secret Dev <131434750+DaDev123@users.noreply.github.com>
2025-02-02 00:15:10 +0100

    Update HideAndSeekMode.cpp

--- source/server/hns/HideAndSeekMode.cpp
+++ source/server/hns/HideAndSeekMode.cpp
@@ -286,11 +286,12 @@ void HideAndSeekMode::update() {
 
 }
 
-void HideAndSeekMode::updateSpectateCam(PlayerActorBase* playerBase)
+void FreezeTagMode::updateSpectateCam(PlayerActorBase* playerBase)
 {
     //If the specate camera ticket is active, get the camera poser
     al::CameraPoser* curPoser;
     al::CameraDirector* director = mCurScene->getCameraDirector();
+
     if (director) {
         al::CameraPoseUpdater* updater = director->getPoseUpdater(0);
         if (updater && updater->mTicket) {
@@ -302,21 +303,31 @@ void HideAndSeekMode::updateSpectateCam(PlayerActorBase* playerBase)
     if (al::isEqualString(curPoser->getName(), "CameraPoserActorSpectate")) {
         cc::CameraPoserActorSpectate* spectatePoser = (cc::CameraPoserActorSpectate*)curPoser;
         spectatePoser->setPlayer(playerBase);
+
         //Increase or decrease spectate index, followed by clamping it
         int indexDirection = 0;
         if(al::isPadTriggerRight(-1)) indexDirection = 1; //Move index right
         if(al::isPadTriggerLeft(-1)) indexDirection = -1; //Move index left
-        //Force index to increase if your current target changes stages
-        if(mSpectateIndex != -1)
+
+        //Force index to decrease if your current index is higher than runner player count
+        //Force index towards -1 during endgame if spectate index is not already -1
+        if(mSpectateIndex >= mInfo->isIt.size() || (mSpectateIndex != -1))
+            indexDirection = -1;
+
+        //Force index to decrease if your current target changes stages
+        if(mSpectateIndex != -1 && indexDirection == 0)
             if(!mInfo->isIt.at(mSpectateIndex)->isInSameStage)
-                indexDirection = 1; //Move index right
+                indexDirection = -1; //Move index left
+
         //Loop over indexs until you find a sutible one in the same stage
         bool isFinalIndex = false;
         while(!isFinalIndex) {
             mSpectateIndex += indexDirection;
+
             // Start by clamping the index
             if(mSpectateIndex < -1) mSpectateIndex = mInfo->isIt.size() - 1;
             if(mSpectateIndex >= mInfo->isIt.size()) mSpectateIndex = -1;
+
             // If not in same stage, skip
             if(mSpectateIndex != -1) {
                 if(mInfo->isIt.at(mSpectateIndex)->isInSameStage)
@@ -324,20 +335,20 @@ void HideAndSeekMode::updateSpectateCam(PlayerActorBase* playerBase)
             } else {
                 isFinalIndex = true;
             }
+
         }
         
         //If no index change is happening, end here
         if(mPrevSpectateIndex == mSpectateIndex)
             return;
+
         //Apply index to target actor and HUD
         if(mSpectateIndex == -1) {
             spectatePoser->setTargetActor(al::getTransPtr(playerBase));
         } else {
             spectatePoser->setTargetActor(&mInfo->isIt.at(mSpectateIndex)->playerPos);
-           }
+        }
+
         mPrevSpectateIndex = mSpectateIndex;
     }
 }
-
-
-// Hooks
