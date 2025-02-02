559591fe71c4911bfca77393c57f2a53e2caf0e6
Secret Dev <131434750+DaDev123@users.noreply.github.com>
2025-02-02 18:25:50 +0100

    Delete include directory

--- include/game/Sequence/SequenceFactory.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-class SequenceFactory {
-public:
-    static void createSequence(const char *);
-};
--- include/packets/PlayerDC.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include "Packet.h"
-
-struct PACKED PlayerDC : Packet {
-    PlayerDC() : Packet() {this->mType = PacketType::PLAYERDC; mPacketSize = sizeof(PlayerDC) - sizeof(Packet);};
-};
