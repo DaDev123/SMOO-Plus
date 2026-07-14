09ec3e99f31c50a048b96b77542a74596f452304
MrKatzenGaming <max@langenhan2.de>
2026-07-14 12:45:45 +0200

    imgui stuff

--- include/main.hpp
+++ include/main.hpp
@@ -8,7 +8,6 @@
 
 #include "basis/seadNew.h"
 #include "heap/seadHeap.h"
-#include "imconfig.h"  // IWYU pragma: keep
 
 inline bool isInGame = false;
 inline bool debugMode = false;
@@ -19,10 +18,8 @@ inline sead::Heap* gHeap = nullptr;
 static int pInfSendTimer = 0;
 static int gameInfSendTimer = 0;
 static int debugPuppetIndex = 0;
-static int debugCaptureIndex = 0;
 static int pageIndex = 0;
 static const int maxPages = 4;
-static char chatInput[0x100] = "";
 
 static constexpr size extraRAMAmount = 4_MB;
 static_assert((extraRAMAmount / 1_MB) % 2 == 0, "Extra RAM amount must be multiple of 2");
