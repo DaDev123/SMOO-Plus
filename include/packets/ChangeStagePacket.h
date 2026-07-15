87c71311a48e9fdf3faf0c9da3ce910f04e85962
egg guy <thecrackingeggguy@gmail.com>
2026-07-15 13:33:34 -0400

    add 2 bytes to ChangeStagePacket to match server

--- include/packets/ChangeStagePacket.h
+++ include/packets/ChangeStagePacket.h
@@ -11,4 +11,5 @@ struct __attribute__((packed)) ChangeStagePacket : Packet {
     char changeID[0x10] = {};
     s8 scenarioNo = -1;
     u8 subScenarioType = -1;
+    u16 extraDataFromServer = 0;
 };
