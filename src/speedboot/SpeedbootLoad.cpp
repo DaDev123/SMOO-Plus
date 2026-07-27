2ac6f7a9e80477fa1b408f65e189a7c66bccf1b7
MrKatzenGaming <max@langenhan2.de>
2026-07-27 16:14:19 +0200

    final changes

--- src/speedboot/SpeedbootLoad.cpp
+++ src/speedboot/SpeedbootLoad.cpp
@@ -183,8 +183,6 @@ void SpeedbootLoad::updateTextElements() {
     al::setPaneStringFormat(this, "TxtCurModVersion", "%s\n", BUILDVER);
     al::setPaneString(this, "TxtCredits", u"CONTRIBUTORS", 0);
     al::setPaneString(this, "TxtContributors",
-                      u"MrKatzenGaming\n"
-                      u"egg guy\n"
                       u"KleinTimmi\n"
                       u"Neorix09\n"
                       u"KGamer77\n"
